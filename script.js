5e92084599b981215fa5b75e8ffbc56c268cad00
Mannu <temp83725@gmail.com>
2025-02-08 11:18:36 +0530

    update

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const buttons = document.querySelectorAll('.buttons button');
 let winner = document.querySelector('.winner');
-let userWinCount = 0
-let compWinCount = 0
+let score = JSON.parse(localStorage.getItem('score')) ||
+ {
+    'userWinCount' : 0,
+    'compWinCount' : 0,
+    'tie' : 0
+};
+document.querySelector('.userWin').innerHTML = score.userWinCount
+document.querySelector('.compWin').innerHTML = score.compWinCount
+document.querySelector('.tie').innerHTML = score.tie
+
 
 document.querySelector('.reset button').addEventListener('click',()=>{
-    userWinCount=0;compWinCount=0;
-    document.querySelector('.userWin').innerHTML = userWinCount
-    document.querySelector('.compWin').innerHTML = compWinCount
+    localStorage.removeItem('score')
+    document.querySelector('.userWin').innerHTML = 0
+    document.querySelector('.compWin').innerHTML = 0
+    document.querySelector('.tie').innerHTML = 0
     winner.innerHTML = '';
+    score.userWinCount = 0
+    score.compWinCount = 0
+    score.tie = 0
 })
 
 buttons.forEach((choice) => {
@@ -21,10 +33,8 @@ const playGame = (userChoice)=>{
     console.log('compChoice : '+ compChoice)
     if(userChoice === compChoice){
         winner.innerHTML = 'DRAW' 
-        userWinCount += 0.5
-        compWinCount += 0.5
-        document.querySelector('.userWin').innerHTML = userWinCount
-        document.querySelector('.compWin').innerHTML = compWinCount
+        score.tie++ 
+        document.querySelector('.tie').innerHTML = score.tie
     }else{
         let userWin = true
         if(userChoice === 'rock'){
@@ -35,17 +45,22 @@ const playGame = (userChoice)=>{
             userWin = compChoice === 'rock' ? false  : true
         }
         checkWinner(userWin)
+        localStorage.setItem('score',JSON.stringify(score))
     }
 }
 const checkWinner = (win)=>{
     if(win){
         winner.innerHTML = 'You Win!'
-        userWinCount++
-        document.querySelector('.userWin').innerHTML = userWinCount
+        score.userWinCount++
+        document.querySelector('.userWin').innerHTML = score.userWinCount
+        console.log(score)
+
     }else{
         winner.innerHTML = 'Computer Wins!'
-        compWinCount++
-        document.querySelector('.compWin').innerHTML = compWinCount
+        score.compWinCount++
+        document.querySelector('.compWin').innerHTML = score.compWinCount
+        console.log(score)
+
     }
 }
 const genComputerChoice = ()=>{
